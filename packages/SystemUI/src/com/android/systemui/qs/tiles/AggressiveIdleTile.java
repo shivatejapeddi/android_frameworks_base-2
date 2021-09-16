CAF-Extended/frameworks_base

Bringup BaikalOS internals,App profiles and Powesave Settings
Double-line statusbar [1/2]

Change-Id: Iab6e5337d1fdfcce0ccbb712758a531309b21686

fixes

fixes

Add base BaikalOS package

Change-Id: I1aee801b0ac46119a18031e74efd566afb8c50f8

Block significant motion sensor in aggressive or extreme powersave modes

Change-Id: Idb1ab00809f456c680ea47444847581f0bf6cc07

Add core BaikalOS system services

Change-Id: I216e249957774440563fd4801196ac1f41201cf8

fixes

fixes

Bringup BaikalOS internals,App profiles and Powesave Settings

Change-Id: If2c7b9c28d6370110f622e33a7469e7997f54ada

Update internals for BaikalSystemService integration

Change-Id: I1c72bb31429687fd621ad59d6af6c11df29fe615

Introduce BaikalSystemService

Change-Id: Ie5f34d7a9f56682da87f3cd3a8d1aacaf93e7e0e

Baikalify more. Bring new infrastructure.

Change-Id: I0f4e4df8d6e8a14038d976a175abb8b3a99b60b0

Protect baikalos broadcasts

Change-Id: I3bb9d4226333b4b17de7c9c399e55c8fb1de80c9

Add ACTION_PROFILE_CHANGED broadcast

Change-Id: I4bf912c43218c5db00bf67c98205533658f41ac5

import

More baikalos settings

Change-Id: Ib511bdd444bb5520e472b498642d48d31e2853df

Alarm blocker and BaikalStaticService

Change-Id: I2080b1b8b4b9cb5e3de6369ab56e19cad8ca7337

Set GMS uid on start

Change-Id: Ib446602c0aaeaa4e3db2e375dea0738803af5a75

Treat whitelisted apps as persistent.

Change-Id: I1159879a66f16f0565dd15fdbcf52b9eeb35e005

Add BaikalActivityService

Change-Id: If40d7c06d4f05395915b3812b97fa9e15a3106e9

Massive import from Pie

Change-Id: I2b7115ec4f205634e6e942ac0ed061377857b563

Enable aggressive and extreme mode idling

Change-Id: If27d1f4e2c8993c4ca1e587fef419a432981c507

baikal: Add idle modes QS tiles

Change-Id: Ie562d2a171f481c7f66c68c5d1c43a8b18102ef4

Add settings for GMS and Idle

Change-Id: Ide7334c9a03bb0782b10fdb5fe578d95869c9d13

App Profile services

Change-Id: Ic8d2a3662fb36707ec3a26103f878df241704318

Reader Mode

Change-Id: I97a19897de985929490aa76454f66f42f54ebe25

Per app brightness control.

packages/SystemUI/src/com/android/systemui/qs/tiles/AggressiveIdleTile.java
@@ -1,0 +1,128 @@
+/*
+ * Copyright (C) 2018 The OmniROM Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.systemui.qs.tiles;
+
+import android.content.ComponentName;
+import android.content.Intent;
+import android.database.ContentObserver;
+import android.net.Uri;
+import android.os.UserHandle;
+import android.provider.Settings;
+import android.service.quicksettings.Tile;
+
+import com.android.systemui.qs.QSHost;
+import com.android.systemui.qs.tileimpl.QSTileImpl;
+import com.android.systemui.plugins.qs.QSTile.BooleanState;
+import com.android.systemui.R;
+
+import com.android.internal.logging.nano.MetricsProto.MetricsEvent;
+
+public class AggressiveIdleTile extends QSTileImpl<BooleanState> {
+    private boolean mAggressiveIdleDisabled;
+    private boolean mListening;
+    private final Icon mIcon = ResourceIcon.get(R.drawable.ic_qs_aggressive_idle);
+
+    public AggressiveIdleTile(QSHost host) {
+        super(host);
+    }
+
+    @Override
+    public boolean isAvailable() {
+        return true;
+    }
+
+    @Override
+    public BooleanState newTileState() {
+        return new BooleanState();
+    }
+
+    @Override
+    public void handleClick() {
+        mAggressiveIdleDisabled = !mAggressiveIdleDisabled;
+        Settings.Global.putInt(mContext.getContentResolver(),
+                Settings.Global.BAIKALOS_AGGRESSIVE_IDLE, mAggressiveIdleDisabled ? 0 : 1);
+        Settings.Global.putInt(mContext.getContentResolver(),
+                Settings.Global.BAIKALOS_EXTREME_IDLE, 0);
+        Settings.Global.putInt(mContext.getContentResolver(),
+		Settings.Global.FORCED_APP_STANDBY_ENABLED, 0);
+        Settings.Global.putInt(mContext.getContentResolver(),
+                Settings.Global.FORCED_APP_STANDBY_FOR_SMALL_BATTERY_ENABLED, 0);
+
+        refreshState();
+    }
+
+
+    @Override
+    public Intent getLongClickIntent() {
+        /*return new Intent().setComponent(new ComponentName(
+                "com.android.settings",
+                "com.android.settings.Settings$AmbientDisplaySettingsActivity"));*/
+	return null;
+    }
+
+
+    @Override
+    public CharSequence getTileLabel() {
+        return mContext.getString(R.string.quick_settings_aggressive_idle_label);
+    }
+
+    @Override
+    protected void handleUpdateState(BooleanState state, Object arg) {
+        if (state.slash == null) {
+            state.slash = new SlashState();
+        }
+        mAggressiveIdleDisabled = Settings.Global.getInt(mContext.getContentResolver(),
+                Settings.Global.BAIKALOS_AGGRESSIVE_IDLE, 0) == 0;
+        state.icon = mIcon;
+        state.value = mAggressiveIdleDisabled;
+        state.slash.isSlashed = state.value;
+        state.label = mContext.getString(R.string.quick_settings_aggressive_idle_label);
+        if (mAggressiveIdleDisabled) {
+            state.state = Tile.STATE_INACTIVE;
+        } else {
+            state.state = Tile.STATE_ACTIVE;
+        }
+    }
+
+    @Override
+    public int getMetricsCategory() {
+        return MetricsEvent.CAFEX;
+    }
+
+    private ContentObserver mObserver = new ContentObserver(mHandler) {
+        @Override
+        public void onChange(boolean selfChange, Uri uri) {
+            refreshState();
+        }
+    };
+
+    @Override
+    public void handleSetListening(boolean listening) {
+        if (mObserver == null) {
+            return;
+        }
+        if (mListening != listening) {
+            mListening = listening;
+            if (listening) {
+                mContext.getContentResolver().registerContentObserver(
+                        Settings.Global.getUriFor(Settings.Global.BAIKALOS_AGGRESSIVE_IDLE), false, mObserver);
+            } else {
+                mContext.getContentResolver().unregisterContentObserver(mObserver);
+            }
+        }
+    }
+}

shivatejapeddi/android_frameworks_base-2

Change-Id: I201640c87ceea79d899fc0579212849647af83c5

packages/SystemUI/src/com/android/systemui/qs/tiles/AggressiveIdleTile.java
@@ -1,0 +1,128 @@
+/*
+ * Copyright (C) 2018 The OmniROM Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.systemui.qs.tiles;
+
+import android.content.ComponentName;
+import android.content.Intent;
+import android.database.ContentObserver;
+import android.net.Uri;
+import android.os.UserHandle;
+import android.provider.Settings;
+import android.service.quicksettings.Tile;
+
+import com.android.systemui.qs.QSHost;
+import com.android.systemui.qs.tileimpl.QSTileImpl;
+import com.android.systemui.plugins.qs.QSTile.BooleanState;
+import com.android.systemui.R;
+
+import com.android.internal.logging.nano.MetricsProto.MetricsEvent;
+
+public class AggressiveIdleTile extends QSTileImpl<BooleanState> {
+    private boolean mAggressiveIdleDisabled;
+    private boolean mListening;
+    private final Icon mIcon = ResourceIcon.get(R.drawable.ic_qs_aggressive_idle);
+
+    public AggressiveIdleTile(QSHost host) {
+        super(host);
+    }
+
+    @Override
+    public boolean isAvailable() {
+        return true;
+    }
+
+    @Override
+    public BooleanState newTileState() {
+        return new BooleanState();
+    }
+
+    @Override
+    public void handleClick() {
+        mAggressiveIdleDisabled = !mAggressiveIdleDisabled;
+        Settings.Global.putInt(mContext.getContentResolver(),
+                Settings.Global.BAIKALOS_AGGRESSIVE_IDLE, mAggressiveIdleDisabled ? 0 : 1);
+        Settings.Global.putInt(mContext.getContentResolver(),
+                Settings.Global.BAIKALOS_EXTREME_IDLE, 0);
+        Settings.Global.putInt(mContext.getContentResolver(),
+		Settings.Global.FORCED_APP_STANDBY_ENABLED, 0);
+        Settings.Global.putInt(mContext.getContentResolver(),
+                Settings.Global.FORCED_APP_STANDBY_FOR_SMALL_BATTERY_ENABLED, 0);
+
+        refreshState();
+    }
+
+
+    @Override
+    public Intent getLongClickIntent() {
+        /*return new Intent().setComponent(new ComponentName(
+                "com.android.settings",
+                "com.android.settings.Settings$AmbientDisplaySettingsActivity"));*/
+	return null;
+    }
+
+
+    @Override
+    public CharSequence getTileLabel() {
+        return mContext.getString(R.string.quick_settings_aggressive_idle_label);
+    }
+
+    @Override
+    protected void handleUpdateState(BooleanState state, Object arg) {
+        if (state.slash == null) {
+            state.slash = new SlashState();
+        }
+        mAggressiveIdleDisabled = Settings.Global.getInt(mContext.getContentResolver(),
+                Settings.Global.BAIKALOS_AGGRESSIVE_IDLE, 0) == 0;
+        state.icon = mIcon;
+        state.value = mAggressiveIdleDisabled;
+        state.slash.isSlashed = state.value;
+        state.label = mContext.getString(R.string.quick_settings_aggressive_idle_label);
+        if (mAggressiveIdleDisabled) {
+            state.state = Tile.STATE_INACTIVE;
+        } else {
+            state.state = Tile.STATE_ACTIVE;
+        }
+    }
+
+    @Override
+    public int getMetricsCategory() {
+        return MetricsEvent.EXTENSIONS;
+    }
+
+    private ContentObserver mObserver = new ContentObserver(mHandler) {
+        @Override
+        public void onChange(boolean selfChange, Uri uri) {
+            refreshState();
+        }
+    };
+
+    @Override
+    public void handleSetListening(boolean listening) {
+        if (mObserver == null) {
+            return;
+        }
+        if (mListening != listening) {
+            mListening = listening;
+            if (listening) {
+                mContext.getContentResolver().registerContentObserver(
+                        Settings.Global.getUriFor(Settings.Global.BAIKALOS_AGGRESSIVE_IDLE), false, mObserver);
+            } else {
+                mContext.getContentResolver().unregisterContentObserver(mObserver);
+            }
+        }
+    }
+}